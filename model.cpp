f148d04dd8ce08867af87a304572704689e1753b
Mateo Sosa <matsosa@itba.edu.ar>
2024-09-20 13:41:44 -0300

    validMoves

--- model.cpp
+++ model.cpp
@@ -9,212 +9,178 @@
 
 #include "model.h"
 
-void initModel(GameModel &model)
+void initModel(GameModel& model)
 {
-    model.gameOver = true;
+	model.gameOver = true;
 
-    model.playerTime[0] = 0;
-    model.playerTime[1] = 0;
+	model.playerTime[0] = 0;
+	model.playerTime[1] = 0;
 
-    memset(model.board, PIECE_EMPTY, sizeof(model.board));
+	memset(model.board, PIECE_EMPTY, sizeof(model.board));
 }
 
-void startModel(GameModel &model)
+void startModel(GameModel& model)
 {
-    model.gameOver = false;
+	model.gameOver = false;
 
-    model.currentPlayer = PLAYER_BLACK;
+	model.currentPlayer = PLAYER_BLACK;
 
-    model.playerTime[0] = 0;
-    model.playerTime[1] = 0;
-    model.turnTimer = GetTime();
+	model.playerTime[0] = 0;
+	model.playerTime[1] = 0;
+	model.turnTimer = GetTime();
 
-    memset(model.board, PIECE_EMPTY, sizeof(model.board));
-    model.board[BOARD_SIZE / 2 - 1][BOARD_SIZE / 2 - 1] = PIECE_WHITE;
-    model.board[BOARD_SIZE / 2 - 1][BOARD_SIZE / 2] = PIECE_BLACK;
-    model.board[BOARD_SIZE / 2][BOARD_SIZE / 2] = PIECE_WHITE;
-    model.board[BOARD_SIZE / 2][BOARD_SIZE / 2 - 1] = PIECE_BLACK;
+	memset(model.board, PIECE_EMPTY, sizeof(model.board));
+	model.board[BOARD_SIZE / 2 - 1][BOARD_SIZE / 2 - 1] = PIECE_WHITE;
+	model.board[BOARD_SIZE / 2 - 1][BOARD_SIZE / 2] = PIECE_BLACK;
+	model.board[BOARD_SIZE / 2][BOARD_SIZE / 2] = PIECE_WHITE;
+	model.board[BOARD_SIZE / 2][BOARD_SIZE / 2 - 1] = PIECE_BLACK;
 }
 
-Player getCurrentPlayer(GameModel &model)
+Player getCurrentPlayer(GameModel& model)
 {
-    return model.currentPlayer;
+	return model.currentPlayer;
 }
 
-int getScore(GameModel &model, Player player)
+int getScore(GameModel& model, Player player)
 {
-    int score = 0;
+	int score = 0;
 
-    for (int y = 0; y < BOARD_SIZE; y++)
-        for (int x = 0; x < BOARD_SIZE; x++)
-        {
-            if (((model.board[y][x] == PIECE_WHITE) &&
-                 (player == PLAYER_WHITE)) ||
-                ((model.board[y][x] == PIECE_BLACK) &&
-                 (player == PLAYER_BLACK)))
-                score++;
-        }
+	for (int y = 0; y < BOARD_SIZE; y++)
+		for (int x = 0; x < BOARD_SIZE; x++)
+		{
+			if (((model.board[y][x] == PIECE_WHITE) &&
+				(player == PLAYER_WHITE)) ||
+				((model.board[y][x] == PIECE_BLACK) &&
+					(player == PLAYER_BLACK)))
+				score++;
+		}
 
-    return score;
+	return score;
 }
 
-double getTimer(GameModel &model, Player player)
+double getTimer(GameModel& model, Player player)
 {
-    double turnTime = 0;
+	double turnTime = 0;
 
-    if (!model.gameOver && (player == model.currentPlayer))
-        turnTime = GetTime() - model.turnTimer;
+	if (!model.gameOver && (player == model.currentPlayer))
+		turnTime = GetTime() - model.turnTimer;
 
-    return model.playerTime[player] + turnTime;
+	return model.playerTime[player] + turnTime;
 }
 
-Piece getBoardPiece(GameModel &model, Square square)
+Piece getBoardPiece(GameModel& model, Square square)
 {
-    return model.board[square.y][square.x];
+	return model.board[square.y][square.x];
 }
 
-void setBoardPiece(GameModel &model, Square square, Piece piece)
+void setBoardPiece(GameModel& model, Square square, Piece piece)
 {
-    model.board[square.y][square.x] = piece;
+	model.board[square.y][square.x] = piece;
 }
 
 bool isSquareValid(Square square)
 {
-    return (square.x >= 0) &&
-           (square.x < BOARD_SIZE) &&
-           (square.y >= 0) &&
-           (square.y < BOARD_SIZE);
+	return (square.x >= 0) &&
+		(square.x < BOARD_SIZE) &&
+		(square.y >= 0) &&
+		(square.y < BOARD_SIZE);
 }
 
 void getValidMoves(GameModel& model, Moves& validMoves)
 {
-    // To-do: your code goes here...
+	// To-do: your code goes here...
 
-    for (int y = 0; y < BOARD_SIZE; y++)
-        for (int x = 0; x < BOARD_SIZE; x++)
-        {
-            Square move = { x, y };
-            bool finded = false;
-            for (auto& loaded_moves : validMoves) {
-                if (loaded_moves.x == move.x && loaded_moves.y == move.y) {
-                    finded = true;
-                }
-            }
-            if (finded) {
-                continue;
-            }
-            // +++ TEST
-            // Lists all empty squares...
-            /*
-            if (getBoardPiece(model, move) == PIECE_EMPTY)
-                validMoves.push_back(move);*/
-                // --- TEST
-            Player player = getCurrentPlayer(model);
-            Piece pos = getBoardPiece(model, move);
+	for (int y = 0; y < BOARD_SIZE; y++) {
+		for (int x = 0; x < BOARD_SIZE; x++)
+		{
+			Square move = { x, y };
 
-            // Si la posición ya esta ocupada no es un movimiento valido 
-            if (pos != PIECE_EMPTY) {
-                continue;
-            }
-            bool is_in = false;
-            // Si el jugador es el negro
-            if (player == PLAYER_BLACK) {
-                // Recorremos todas las direcciones alrededor de la pieza
-                for (int i = -1; i <= 1 && !is_in; i++) {
-                    for (int j = -1; j <= 1 && !is_in; j++) {
-                        if (j == 0 && i == 0) {
-                            continue;
-                        }
-                        Square check_piece = { x + i, y + j };
+			// +++ TEST
+			// Lists all empty squares...
+			/*
+			if (getBoardPiece(model, move) == PIECE_EMPTY)
+				validMoves.push_back(move);*/
+				// --- TEST
+			Player player = getCurrentPlayer(model);
+			Piece positionState = getBoardPiece(model, move);
 
-                        for (int steps = 0; isSquareValid(check_piece); steps++) {
-                            // Obtenemos una pieza desplazandonos en una dirección
-                            Piece directional_piece = getBoardPiece(model, check_piece);
-                            if (directional_piece == PIECE_EMPTY) {
-                                break;
-                            }
-                            else if (directional_piece == PIECE_BLACK) {
-                                if (steps == 0) {
-                                    break;
-                                }
-                                else {
-                                    
-                                    validMoves.push_back(move);
-                                    is_in = true;
-                                    break;
-                                }
-                            }
-                            // Si la pieza es blanca avanzo en la dirección
-                            check_piece = { x + (steps + 2) * i, y + (steps + 2) * j };
+			// Si la posición ya esta ocupada no es un movimiento valido 
+			if (positionState != PIECE_EMPTY) {
+				continue;
+			}
+			bool movementAlreadyAdded = false;
 
-                        }
-                    }
-                }
-            }
-            else if (player == PLAYER_WHITE) {
-                for (int i = -1; i <= 1 && !is_in; i++) {
-                    for (int j = -1; j <= 1 && !is_in; j++) {
-                        if (j == 0 && i == 0) {
-                            continue;
-                        }
-                        Square check_piece = { x + i, y + j };
-                        bool valid_piece = 0;
-                        for (int steps = 0; isSquareValid(check_piece); steps++) {
-                            // Obtenemos una pieza desplazandonos en una dirección
-                            Piece directional_piece = getBoardPiece(model, check_piece);
-                            if (directional_piece == PIECE_EMPTY) {
-                                break;
-                            }
-                            else if (directional_piece == PIECE_WHITE) {
-                                if (steps == 0) {
-                                    break;
-                                }
-                                else {
-                                    validMoves.push_back(move);
-                                    is_in = true;
-                                    break;
-                                }
-                            }
-                            // Si la pieza es blanca avanzo en la dirección
-                            check_piece = { x + (steps + 2) * i, y + (steps + 2) * j };
+				// Recorremos todas las direcciones alrededor de la pieza
+			for (int i = -1; i <= 1 && !movementAlreadyAdded; i++) {
+				for (int j = -1; j <= 1 && !movementAlreadyAdded; j++) {
+					if (j == 0 && i == 0) {
+						continue;
+					}
+					Square directionalSquare = { x + i, y + j };
+					
+						for (int steps = 0; isSquareValid(directionalSquare) && !movementAlreadyAdded; steps++) {
+							// Obtenemos una pieza desplazandonos en una dirección
+							Piece directionalPiece = getBoardPiece(model, directionalSquare);
+							if (directionalPiece == PIECE_EMPTY) {
+								break;
+							}
+							else if ((directionalPiece == PIECE_BLACK && player == PLAYER_BLACK) ||
+								(directionalPiece == PIECE_WHITE && player == PLAYER_WHITE)) {
 
-                        }
-                    }
-                }
-            }
-            is_in = false;
-        }
+								if (steps == 0) {
+									break;
+								}
+								else {
+									validMoves.push_back(move);
+									movementAlreadyAdded = true;
+									
+								}
+							}
+							// Si la pieza es blanca avanzo en la dirección
+							directionalSquare = { x + (steps + 2) * i, y + (steps + 2) * j };
+
+						
+					}
+
+					
+					
+				}
+			}
+
+		}
+
+	}
 }
 
-bool playMove(GameModel &model, Square move)
+bool playMove(GameModel& model, Square move)
 {
-    // Set game piece
-    Piece piece =
-        (getCurrentPlayer(model) == PLAYER_WHITE)
-            ? PIECE_WHITE
-            : PIECE_BLACK;
+	// Set game piece
+	Piece piece =
+		(getCurrentPlayer(model) == PLAYER_WHITE)
+		? PIECE_WHITE
+		: PIECE_BLACK;
 
-    setBoardPiece(model, move, piece);
+	setBoardPiece(model, move, piece);
 
-    // To-do: your code goes here...
+	// To-do: your code goes here...
 
-    // Update timer
-    double currentTime = GetTime();
-    model.playerTime[model.currentPlayer] += currentTime - model.turnTimer;
-    model.turnTimer = currentTime;
+	// Update timer
+	double currentTime = GetTime();
+	model.playerTime[model.currentPlayer] += currentTime - model.turnTimer;
+	model.turnTimer = currentTime;
 
-    // Swap player
-    model.currentPlayer =
-        (model.currentPlayer == PLAYER_WHITE)
-            ? PLAYER_BLACK
-            : PLAYER_WHITE;
+	// Swap player
+	model.currentPlayer =
+		(model.currentPlayer == PLAYER_WHITE)
+		? PLAYER_BLACK
+		: PLAYER_WHITE;
 
-    // Game over?
-    Moves validMoves;
-    getValidMoves(model, validMoves);
+	// Game over?
+	Moves validMoves;
+	getValidMoves(model, validMoves);
 
-    if (validMoves.size() == 0)
-        model.gameOver = true;
+	if (validMoves.size() == 0)
+		model.gameOver = true;
 
-    return true;
+	return true;
 }
