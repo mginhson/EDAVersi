07f2b4915a9c1b51e724382e646409e5918e855a
mginhson <mginhson@itba.edu.ar>
2024-09-23 18:57:42 -0300

    m

--- ai.cpp
+++ ai.cpp
@@ -13,14 +13,16 @@
 #include "controller.h"
 #include "model.h"
 #define MAX_NODE_COUNT 5000
+unsigned int nodeCount = 0;
 
 typedef struct{
     float alpha, beta;
 }Pruning_t;
 
+#define MAX_DEPTH 4
 static Pruning_t minMaxTraverse (GameModel model, float alpha, float beta)
 {
-    
+
 }
 
 
@@ -58,29 +60,8 @@ static float evaluateNode(GameModel& currentModel)
 
 
 
-static void buildTree(Tree_Nodes_t& currentState, unsigned int levelCount) {
-    Tree_Nodes_t placeholder;
-    if (levelCount == 0) {
-        return;
-    }
-    Moves validMoves;
-    getValidMoves(currentState.proposedGameModel, validMoves);
-    for (auto &move : validMoves) {
-        
-        
-        placeholder.proposedGameModel = currentState.proposedGameModel;
-        placeholder.previousMovement = move;
-        playMove(placeholder.proposedGameModel, move);
-        currentState.nextStates.push_front(placeholder);
 
-    }
-    for (auto &node : currentState.nextStates) {
-        buildTree(node, levelCount - 1);
-    }
-    
-}
 
-#define BRANCHES_LEVEL 4
 
 
 
