f6203d796789f163668a239952f8b160b23d20b7
mginhson <mginhson@itba.edu.ar>
2024-09-23 19:27:15 -0300

    m

--- ai.cpp
+++ ai.cpp
@@ -6,8 +6,8 @@
  */
 
 #include <cstdlib>
-#include <forward_list>
-#include <iostream>
+#include <limits>
+
 
 #include "ai.h"
 #include "controller.h"
@@ -15,14 +15,65 @@
 #define MAX_NODE_COUNT 5000
 unsigned int nodeCount = 0;
 
-typedef struct{
-    float alpha, beta;
-}Pruning_t;
+
 
 #define MAX_DEPTH 4
-static Pruning_t minMaxTraverse (GameModel model, float alpha, float beta)
+static float minMaxTraverse (GameModel model, float alpha, float beta, int remainingLevels)
 {
+    Pruning_t placeholder;
 
+    float bestScore;
+
+    if (remainingLevels == 0)
+    {
+        //evaluate
+    }
+    
+    //We have to minimize
+    if (model.currentPlayer == model.humanPlayer)
+    {
+        float value;
+        bestScore =  std::numeric_limits<double>::infinity(); //-INF
+        Moves validMovements;
+        getValidMoves(model, validMovements);
+        GameModel copiedModel = model;
+        for (auto movement : validMovements)
+        {
+            copiedModel = model;
+            playMove(copiedModel, movement);
+            value = minMaxTraverse(copiedModel, alpha, beta, remainingLevels - 1);
+            bestScore = std::min(value, bestScore);
+            beta = std::min(beta, bestScore);
+
+            //AlphaBeta condition
+            if (alpha >= beta)
+                break;
+            
+
+        }
+    }
+    else //We have to maximize, it's AI player
+    {
+        float value;
+        bestScore = -std::numeric_limits<double>::infinity();
+        Moves validMovements;
+        getValidMoves (model, validMovements);
+        GameModel copiedModel;
+
+        for (auto movement : validMovements)
+        {
+            copiedModel = model;
+            playMove (copiedModel,movement);
+            value = minMaxTraverse(copiedModel, alpha, beta, remainingLevels - 1);
+            bestScore = std::max(bestScore, value);
+            alpha = std::max(alpha, bestScore);
+
+            if (alpha >= beta)
+                break;
+        }
+    }
+
+    return bestScore;
 }
 
 
